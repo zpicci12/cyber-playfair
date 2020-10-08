69f688b6d463283a7bc4b0e68732f2778a57b24f
Zoe <zpiccirillo10@stuy.edu>
2020-10-08 16:11:54 -0400

    created method to split ciphertext into pairs for encoding, adding x's where necessary

--- PlayfairEncode.java
+++ PlayfairEncode.java
@@ -1,8 +1,9 @@
 public class PlayfairEncode {
   public static void main(String[] args){
-    String text = args[0]; //the text to be encoded
+    String text = args[0].toUpperCase(); //the text to be encoded
     String keyText = args[1]; //the key letters for encoding
     String [][] key = makeKey(keyText); //store key letters in a 5x5 array
+    String[] pairs = makePairs(text); //insert x's if needed and return the pairs for encoding
   }
 
   //store key letters in a 5x5 array
@@ -17,5 +18,30 @@ public class PlayfairEncode {
       }
     }
     return key;
+  }
+
+  //make the two-letter pairs used to encode the text; return as an array
+  public static String[] makePairs(String text){
+    String [] pairs = new String [text.length()]; //extra indexes for when x's are added
+    int n = 0;
+    int count = 0;
+    //go through text in pairs of 2; add x's where needed
+    while (n < text.length()){
+      String pair = text.substring(n, n + 2);
+      //if the pair is a double letter, add an x and increment n by 1 to keep the pairs in order
+      if (pair.substring(0, 1).equals(pair.substring(1))){
+        String newPair = pair.substring(0, 1) + 'X';
+        n += 1;
+        pairs[count] = newPair;
+        count++;
+      }
+      //else don't change the pair
+      else {
+      pairs[count] = pair;
+      count++;
+      n += 2;
+      }
+    }
+    return pairs;
   }
 }
