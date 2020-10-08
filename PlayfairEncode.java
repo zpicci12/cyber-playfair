e872a18617ed774588a3706bd6b80333271e7f8a
Zoe <zpiccirillo10@stuy.edu>
2020-10-08 15:31:54 -0400

    created function to convert key text to a 5x5 array

--- PlayfairEncode.java
+++ PlayfairEncode.java
@@ -1,9 +1,21 @@
-//import java.io.File;
-
 public class PlayfairEncode {
-  public static void main(String[]args){
-  //  System.out.print("hello world");
-    //File
-    //File toEncode = new File(args[0]);
+  public static void main(String[] args){
+    String text = args[0]; //the text to be encoded
+    String keyText = args[1]; //the key letters for encoding
+    String [][] key = makeKey(keyText); //store key letters in a 5x5 array
+  }
+
+  //store key letters in a 5x5 array
+  public static String[][] makeKey(String keyText){
+    String [][] key = new String [5][5];
+    int count = 0;
+    for (int i = 0; i < key.length; i++){
+      for (int j = 0; j < key[0].length; j++){
+        String letter = keyText.substring(count, count + 1);
+        key[i][j] = letter;
+        count++;
+      }
+    }
+    return key;
   }
 }
