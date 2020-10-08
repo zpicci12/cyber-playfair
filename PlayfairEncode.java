f1a8e9ad2e9676c4f19dd3e52eb2b6622d73e824
Zoe <zpiccirillo10@stuy.edu>
2020-10-08 14:49:31 -0400

    create encode class

---
+++ PlayfairEncode.java
@@ -0,0 +1,9 @@
+//import java.io.File;
+
+public class PlayfairEncode {
+  public static void main(String[]args){
+  //  System.out.print("hello world");
+    //File
+    //File toEncode = new File(args[0]);
+  }
+}
